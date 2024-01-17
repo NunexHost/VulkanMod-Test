120ad91dd2d6b1274488a5ae9c8ca14af66e2b64
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-17 12:23:19 -0300

    Update SpriteUtil.java

--- src/main/java/net/vulkanmod/render/texture/SpriteUtil.java
+++ src/main/java/net/vulkanmod/render/texture/SpriteUtil.java
@@ -4,7 +4,7 @@ import net.vulkanmod.vulkan.texture.VulkanImage;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.vulkan.VkCommandBuffer;
 
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Set;
 
 public abstract class SpriteUtil {
@@ -26,9 +26,17 @@ public abstract class SpriteUtil {
     }
 
     public static void transitionLayouts(VkCommandBuffer commandBuffer) {
-        try(MemoryStack stack = MemoryStack.stackPush()) {
-            transitionedLayouts.forEach(image -> image.readOnlyLayout(stack, commandBuffer));
+        // Reutilize uma instância única de MemoryStack
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            // Agrupe transições em um único comando
+            int[] imageIds = transitionedLayouts.stream().mapToInt(image -> image.getId()).toArray();
+            commandBuffer.pipelineBarrier(VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT,
+                    0, null, null, Arrays.stream(imageIds).mapToObj(imageId ->
+                            new VkImageMemoryBarrier(VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+                                    0, 0, imageId, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL))
+                            .toArray(VkImageMemoryBarrier[]::new));
 
+            // Limpe a lista de transições
             transitionedLayouts.clear();
         }
     }
