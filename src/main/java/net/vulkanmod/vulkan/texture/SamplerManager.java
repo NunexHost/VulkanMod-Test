d2b5a5f2016ba1598702a8b9d0401dd8637d5903
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-18 12:50:43 -0300

    Update SamplerManager.java

--- src/main/java/net/vulkanmod/vulkan/texture/SamplerManager.java
+++ src/main/java/net/vulkanmod/vulkan/texture/SamplerManager.java
@@ -26,7 +26,7 @@ public abstract class SamplerManager {
         short key = (short) (flags | (mipLevels << 8));
         long sampler = SAMPLERS.getOrDefault(key, 0L);
 
-        if(sampler == 0L) {
+        if (sampler == 0L) {
             sampler = createTextureSampler(mipLevels, flags);
             SAMPLERS.put(key, sampler);
         }
@@ -35,29 +35,27 @@ public abstract class SamplerManager {
     }
 
     private static long createTextureSampler(byte mipLevels, byte flags) {
-        Validate.isTrue(
-                (flags & (REDUCTION_MIN_BIT | REDUCTION_MAX_BIT)) != (REDUCTION_MIN_BIT | REDUCTION_MAX_BIT)
-        );
+        Validate.isTrue((flags & (REDUCTION_MIN_BIT | REDUCTION_MAX_BIT)) != (REDUCTION_MIN_BIT | REDUCTION_MAX_BIT));
 
-        try(MemoryStack stack = stackPush()) {
+        try (MemoryStack stack = stackPush()) {
 
             VkSamplerCreateInfo samplerInfo = VkSamplerCreateInfo.calloc(stack);
             samplerInfo.sType(VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO);
 
-            samplerInfo.magFilter(flags & LINEAR_FILTERING_BIT != 0 ? VK_FILTER_LINEAR : VK_FILTER_NEAREST);
-            samplerInfo.minFilter(flags & LINEAR_FILTERING_BIT != 0 ? VK_FILTER_LINEAR : VK_FILTER_NEAREST);
+            samplerInfo.magFilter((flags & LINEAR_FILTERING_BIT) != 0 ? VK_FILTER_LINEAR : VK_FILTER_NEAREST);
+            samplerInfo.minFilter((flags & LINEAR_FILTERING_BIT) != 0 ? VK_FILTER_LINEAR : VK_FILTER_NEAREST);
 
-            samplerInfo.addressModeU(flags & CLAMP_BIT != 0 ? VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE : VK_SAMPLER_ADDRESS_MODE_REPEAT);
-            samplerInfo.addressModeV(flags & CLAMP_BIT != 0 ? VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE : VK_SAMPLER_ADDRESS_MODE_REPEAT);
-            samplerInfo.addressModeW(flags & CLAMP_BIT != 0 ? VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE : VK_SAMPLER_ADDRESS_MODE_REPEAT);
+            samplerInfo.addressModeU((flags & CLAMP_BIT) != 0 ? VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE : VK_SAMPLER_ADDRESS_MODE_REPEAT);
+            samplerInfo.addressModeV((flags & CLAMP_BIT) != 0 ? VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE : VK_SAMPLER_ADDRESS_MODE_REPEAT);
+            samplerInfo.addressModeW((flags & CLAMP_BIT) != 0 ? VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE : VK_SAMPLER_ADDRESS_MODE_REPEAT);
 
-            samplerInfo.mipmapMode(flags & USE_MIPMAPS_BIT != 0 ? VK_SAMPLER_MIPMAP_MODE_LINEAR : VK_SAMPLER_MIPMAP_MODE_NEAREST);
+            samplerInfo.mipmapMode((flags & USE_MIPMAPS_BIT) != 0 ? VK_SAMPLER_MIPMAP_MODE_LINEAR : VK_SAMPLER_MIPMAP_MODE_NEAREST);
             samplerInfo.maxLod(mipLevels);
             samplerInfo.minLod(0.0F);
             samplerInfo.mipLodBias(MIP_BIAS);
 
             // Reduction Mode
-            if((flags & (REDUCTION_MAX_BIT | REDUCTION_MIN_BIT)) != 0) {
+            if ((flags & (REDUCTION_MIN_BIT | REDUCTION_MAX_BIT)) != 0) {
                 VkSamplerReductionModeCreateInfo reductionModeInfo = VkSamplerReductionModeCreateInfo.calloc(stack);
                 reductionModeInfo.sType$Default();
                 reductionModeInfo.reductionMode((flags & REDUCTION_MAX_BIT) != 0 ? VK_SAMPLER_REDUCTION_MODE_MAX : VK_SAMPLER_REDUCTION_MODE_MIN);
@@ -66,18 +64,13 @@ public abstract class SamplerManager {
 
             LongBuffer pTextureSampler = stack.mallocLong(1);
 
-            if(vkCreateSampler(getDevice(), samplerInfo, null, pTextureSampler) != VK_SUCCESS) {
+            if (vkCreateSampler(getDevice(), samplerInfo, null, pTextureSampler) != VK_SUCCESS) {
                 throw new RuntimeException("Failed to create texture sampler");
             }
 
             return pTextureSampler.get(0);
         }
-     }
-
-    public static void cleanUp() {
-        for(long id : SAMPLERS.values()) {
-            vkDestroySampler(DeviceManager.device, id, null);
-        }
+        
     }
 
     public static final byte LINEAR_FILTERING_BIT = 1;
