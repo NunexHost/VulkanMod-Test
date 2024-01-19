8153040585b016c2f2ac932587f86e0d76a2f9ea
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-18 21:30:15 -0300

    Update AutoIndexBuffer.java

--- src/main/java/net/vulkanmod/vulkan/memory/AutoIndexBuffer.java
+++ src/main/java/net/vulkanmod/vulkan/memory/AutoIndexBuffer.java
@@ -1,94 +1,71 @@
-package net.vulkanmod.vulkan.memory;
+package net.vulkanmod.vulkan.shader.layout;
 
-import org.lwjgl.system.MemoryUtil;
+import net.vulkanmod.vulkan.shader.descriptor.UBO;
 
-import java.nio.ByteBuffer;
-import java.nio.ShortBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AutoIndexBuffer {
-    int vertexCount;
-    final DrawType drawType;
-    IndexBuffer indexBuffer;
+public abstract class AlignedStruct {
 
-    public AutoIndexBuffer(int vertexCount, DrawType type) {
-        this.drawType = type;
+    protected final List<Field> fields = new ArrayList<>();
+    protected final int size;
 
-        createIndexBuffer(vertexCount);
-    }
+    protected AlignedStruct(List<Field.FieldInfo> infoList, int size) {
+        this.size = size;
 
-    private void createIndexBuffer(int vertexCount) {
-        this.vertexCount = vertexCount;
-        int size;
-        ByteBuffer buffer;
+        if (infoList == null) return;
 
-        switch (drawType) {
-            case QUADS -> {
-                size = vertexCount * 3 / 2 * IndexBuffer.IndexType.SHORT.size;
-                buffer = directAlloc(size);
-            }
-            case TRIANGLE_FAN -> {
-                size = (vertexCount - 2) * 3 * IndexBuffer.IndexType.SHORT.size;
-                buffer = directAlloc(size);
-            }
-            case TRIANGLE_STRIP -> {
-                size = (vertexCount - 2) * 3 * IndexBuffer.IndexType.SHORT.size;
-                buffer = directAlloc(size);
-            }
-            default -> throw new RuntimeException("unknown drawType");
+        for (Field.FieldInfo fieldInfo : infoList) {
+            Field field = Field.createField(fieldInfo);
+            this.fields.add(field);
         }
 
-        indexBuffer = new IndexBuffer(buffer, MemoryTypes.GPU_MEM);
-        indexBuffer.copyBuffer(buffer);
-
-        MemoryUtil.memFree(buffer);
+        // Sort fields by size (assuming compareSizes method exists)
+        this.fields.sort(Field::compareSizes);
     }
 
-    public void checkCapacity(int vertexCount) {
-        if(vertexCount > this.vertexCount) {
-            int newVertexCount = this.vertexCount * 2;
-            System.out.println("Reallocating AutoIndexBuffer from " + this.vertexCount + " to " + newVertexCount);
-
-            //TODO: free old
-            //Can't know when VBO will stop using it
-            indexBuffer.freeBuffer();
-
-            // Reallocate with pre-calculated indices
-            buffer = precalculatedIndices(newVertexCount);
-            indexBuffer = new IndexBuffer(buffer, MemoryTypes.GPU_MEM);
-            indexBuffer.copyBuffer(buffer);
-
-            MemoryUtil.memFree(buffer);
+    public void update(long ptr) {
+        for (Field field : this.fields) {
+            field.update(ptr);
         }
     }
 
-    private static ByteBuffer precalculatedIndices(int vertexCount) {
-        //short[] idxs = {0, 1, 2, 0, 2, 3};
+    public List<Field> getFields() {
+        return this.fields;
+    }
 
-        int indexCount = vertexCount * 3 / 2;
-        ByteBuffer buffer = directAlloc(indexCount * Short.BYTES);
-        ShortBuffer idxs = buffer.asShortBuffer();
+    public int getSize() {
+        return size;
+    }
 
-        int j = 0;
-        for(int i = 0; i < vertexCount; i += 4) {
+    public static class Builder {
 
-            idxs.put(j, (short) i);
-            idxs.put(j + 1, (short) (i + 1));
-            idxs.put(j + 2, (short) (i + 2));
-            idxs.put(j + 3, (short) (i));
-            idxs.put(j + 4, (short) (i + 2));
-            idxs.put(j + 5, (short) (i + 3));
+        final List<Field.FieldInfo> fields = new ArrayList<>();
+        protected int currentOffset = 0;
 
-            j += 6;
+        public void addFieldInfo(String type, String name, int count) {
+            Field.FieldInfo fieldInfo = Field.createFieldInfo(type, name, count);
+
+            // Pre-compute alignment offset
+            fieldInfo.alignmentOffset = fieldInfo.computeAlignmentOffset(currentOffset);
+            currentOffset = fieldInfo.alignmentOffset + fieldInfo.size;
+            fields.add(fieldInfo);
         }
 
-        return buffer;
-    }
+        public void addFieldInfo(String type, String name) {
+            addFieldInfo(type, name, 1);
+        }
 
-    private static ByteBuffer directAlloc(int size) {
-        return MemoryUtil.memAlloc(size, MemoryType.HOST_VISIBLE, MemoryType.HOST_COHERENT);
-    }
+        public UBO buildUBO(int binding, int stages) {
+            return new UBO(binding, stages, currentOffset * 4, fields);
+        }
 
-    public IndexBuffer getIndexBuffer() { return indexBuffer; }
+        public PushConstants buildPushConstant() {
+            if (fields.isEmpty()) return null;
+
+            return new PushConstants(fields, currentOffset * 4);
+        }
+    }
 
     public enum DrawType {
         QUADS(7),
