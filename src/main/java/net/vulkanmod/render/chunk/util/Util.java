eb1dde9f0e81c981467291f60438c781f98c8402
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-19 13:37:26 -0300

    Update Util.java

--- src/main/java/net/vulkanmod/render/chunk/util/Util.java
+++ src/main/java/net/vulkanmod/render/chunk/util/Util.java
@@ -1,5 +1,3 @@
-package net.vulkanmod.render.chunk.util;
-
 import net.minecraft.core.Direction;
 import org.joml.Matrix4f;
 import org.lwjgl.system.MemoryStack;
@@ -11,23 +9,16 @@ import java.nio.FloatBuffer;
 public class Util {
 
     public static final Direction[] DIRECTIONS = Direction.values();
-    public static final Direction[] XZ_DIRECTIONS = getXzDirections();
 
-    private static Direction[] getXzDirections() {
-        Direction[] directions = new Direction[4];
-
-        int i = 0;
-        for(Direction direction : Direction.values()) {
-            if(direction.getAxis() == Direction.Axis.X || direction.getAxis() == Direction.Axis.Z) {
-                directions[i] = direction;
-                ++i;
-            }
-        }
-        return directions;
-    }
+    private static final Direction[] XZ_DIRECTIONS = new Direction[]{
+        Direction.NORTH,
+        Direction.SOUTH,
+        Direction.EAST,
+        Direction.WEST
+    };
 
     public static long posLongHash(int x, int y, int z) {
-        return (long)x & 0x00000000FFFFL | ((long) z << 16) & 0x0000FFFF0000L | ((long) y << 32) & 0xFFFF00000000L;
+        return (long) x | ((long) z << 16) | ((long) y << 32);
     }
 
     public static int flooredLog(int v) {
@@ -35,7 +26,7 @@ public class Util {
         int log = 30;
         int t = 0x40000000;
 
-        while((v & t) == 0) {
+        while (t > v) {
             t >>= 1;
             log--;
         }
@@ -44,13 +35,14 @@ public class Util {
     }
 
     public static int align(int i, int alignment) {
-        int r = i % alignment;
-        return r != 0 ? i + alignment - r : i;
+        return i + alignment - (i % alignment);
     }
 
     public static ByteBuffer createCopy(ByteBuffer src) {
-        ByteBuffer ret = MemoryUtil.memAlloc(src.remaining());
-        MemoryUtil.memCopy(src, ret);
-        return ret;
+        if (src.isDirect()) {
+            return src.duplicate();
+        } else {
+            return MemoryUtil.memSlice(src);
+        }
     }
 }
