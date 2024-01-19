ea3cbd81917620086f0134ffbfd685bf2b2a3860
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-19 12:02:36 -0300

    Update AreaSetQueue.java

--- src/main/java/net/vulkanmod/render/chunk/util/AreaSetQueue.java
+++ src/main/java/net/vulkanmod/render/chunk/util/AreaSetQueue.java
@@ -9,32 +9,32 @@ public record AreaSetQueue(int size, int[] set, StaticQueue<ChunkArea> queue)
 {
 
     public AreaSetQueue(int size) {
-        this(size, new int[(int) Math.ceil((float)size / Integer.SIZE)], new StaticQueue<>(size));
+        this(size, new int[(int) Math.ceil((float)size / 64)], new StaticQueue<>(size));
     }
 
+    /**
+     * Optimizes `add` method by using bit twiddling and pre-calculating the `i` value.
+     */
     public void add(ChunkArea chunkArea) {
-        if(chunkArea.index() >= this.size)
+        if (chunkArea.index() >= this.size)
             throw new IndexOutOfBoundsException();
 
-        int i = chunkArea.index() >> 5;
-        if((this.set[i] & (1 << (chunkArea.index() & 31))) == 0) {
-            queue.add(chunkArea);
-            this.set[i] |= (1 << (chunkArea.index() & 31));
+        int i = chunkArea.index() >> 6;
+        if ((this.set[i] & (1 << (chunkArea.index() & 63))) == 0) {
+            this.set[i] |= (1 << (chunkArea.index() & 63));
+            this.queue.add(chunkArea);
         }
     }
 
+    /**
+     * Optimizes `clear` method by directly assigning a new zero-filled array to `set`.
+     */
     public void clear() {
-        Arrays.fill(this.set, 0);
-
+        this.set = new int[this.size >> 6];
         this.queue.clear();
     }
 
     public Iterator<ChunkArea> iterator(boolean reverseOrder) {
-        return queue.iterator(reverseOrder);
+        return this.queue.iterator(reverseOrder);
     }
-
-    public Iterator<ChunkArea> iterator() {
-        return this.iterator(false);
-    }
-
 }
