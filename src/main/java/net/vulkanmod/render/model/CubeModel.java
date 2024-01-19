59cd913390b5c9c19d22073239a6613dd2caec59
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-18 22:25:18 -0300

    Update CubeModel.java

--- src/main/java/net/vulkanmod/render/model/CubeModel.java
+++ src/main/java/net/vulkanmod/render/model/CubeModel.java
@@ -21,6 +21,11 @@ public class CubeModel {
     final Vector3f[] transformed = new Vector3f[8];
 
     public void setVertices(int i, int j, float f, float g, float h, float k, float l, float m, float n, float o, float p, boolean bl, float q, float r, Set<Direction> set) {
+
+        // Pre-calculate 16.0f division
+
+        final float div = 1.0f / 16.0f;
+
         this.minX = f;
         this.minY = g;
         this.minZ = h;
@@ -28,6 +33,7 @@ public class CubeModel {
         this.maxY = g + l;
         this.maxZ = h + m;
         this.polygons = new ModelPart.Polygon[set.size()];
+
         float s = maxX;
         float t = maxY;
         float u = maxZ;
@@ -44,72 +50,38 @@ public class CubeModel {
         }
 
         this.vertices = new Vector3f[]{
-                new Vector3f(f, g, h),
-                new Vector3f(s, g, h),
-                new Vector3f(s, t, h),
-                new Vector3f(f, t, h),
-                new Vector3f(f, g, u),
-                new Vector3f(s, g, u),
-                new Vector3f(s, t, u),
-                new Vector3f(f, t, u)
+            new Vector3f(f, g, h),
+            new Vector3f(s, g, h),
+            new Vector3f(s, t, h),
+            new Vector3f(f, t, h),
+            new Vector3f(f, g, u),
+            new Vector3f(s, g, u),
+            new Vector3f(s, t, u),
+            new Vector3f(f, t, u)
         };
 
-        for (int i1 = 0; i1 < 8; i1++) {
-            //pre-divide all vertices once
-            this.vertices[i1].div(16.0f);
-            this.transformed[i1] = new Vector3f(0.0f);
+        // Transform original vertices and store them
+
+        for (int i1 = 0; i1 < 8; ++i1) {
+            this.vertices[i1].mul(div);
+            this.transformed[i1] = this.vertices[i1];
         }
 
-        ModelPart.Vertex vertex1 = new ModelPart.Vertex(transformed[0], 0.0F, 0.0F);
-        ModelPart.Vertex vertex2 = new ModelPart.Vertex(transformed[1], 0.0F, 8.0F);
-        ModelPart.Vertex vertex3 = new ModelPart.Vertex(transformed[2], 8.0F, 8.0F);
-        ModelPart.Vertex vertex4 = new ModelPart.Vertex(transformed[3], 8.0F, 0.0F);
-        ModelPart.Vertex vertex5 = new ModelPart.Vertex(transformed[4], 0.0F, 0.0F);
-        ModelPart.Vertex vertex6 = new ModelPart.Vertex(transformed[5], 0.0F, 8.0F);
-        ModelPart.Vertex vertex7 = new ModelPart.Vertex(transformed[6], 8.0F, 8.0F);
-        ModelPart.Vertex vertex8 = new ModelPart.Vertex(transformed[7], 8.0F, 0.0F);
+        // Create vertices for each face
 
-        float w = (float)i;
-        float x = (float)i + m;
-        float y = (float)i + m + k;
-        float z = (float)i + m + k + k;
-        float aa = (float)i + m + k + m;
-        float ab = (float)i + m + k + m + k;
-        float ac = (float)j;
-        float ad = (float)j + m;
-        float ae = (float)j + m + l;
+        ModelPart.Vertex vertex1 = new ModelPart.Vertex(this.transformed[0], 0.0F, 0.0F);
+        ModelPart.Vertex vertex2 = new ModelPart.Vertex(this.transformed[1], 0.0F, 8.0F);
+        ModelPart.Vertex vertex3 = new ModelPart.Vertex(this.transformed[2], 8.0F, 8.0F);
+        ModelPart.Vertex vertex4 = new ModelPart.Vertex(this.transformed[3], 8.0F, 0.0F);
+
+        // Create polygons for each face
+
         int idx = 0;
         if (set.contains(Direction.DOWN)) {
-            this.polygons[idx++] = new ModelPart.Polygon(new ModelPart.Vertex[]{vertex6, vertex5, vertex1, vertex2}, x, ac, y, ad, q, r, bl, Direction.DOWN);
+            this.polygons[idx++] = new ModelPart.Polygon(new ModelPart.Vertex[]{vertex1, vertex2, vertex3, vertex4}, this.transformed[0].x, this.transformed[0].y, this.transformed[0].z, this.transformed[1].x, this.transformed[1].y, this.transformed[1].z, bl, Direction.DOWN);
         }
 
         if (set.contains(Direction.UP)) {
-            this.polygons[idx++] = new ModelPart.Polygon(new ModelPart.Vertex[]{vertex3, vertex4, vertex8, vertex7}, y, ad, z, ac, q, r, bl, Direction.UP);
+            this.polygons[idx++] = new ModelPart.Polygon(new ModelPart.Vertex[]{vertex4, vertex3, vertex2, vertex1}, this.transformed[4].x, this.transformed[4].y, this.transformed[4].z, this.transformed[5].x, this.transformed[5].y, this.transformed[5].z, bl, Direction.UP);
         }
-
-        if (set.contains(Direction.WEST)) {
-            this.polygons[idx++] = new ModelPart.Polygon(new ModelPart.Vertex[]{vertex1, vertex5, vertex8, vertex4}, w, ad, x, ae, q, r, bl, Direction.WEST);
-        }
-
-        if (set.contains(Direction.NORTH)) {
-            this.polygons[idx++] = new ModelPart.Polygon(new ModelPart.Vertex[]{vertex2, vertex1, vertex4, vertex3}, x, ad, y, ae, q, r, bl, Direction.NORTH);
-        }
-
-        if (set.contains(Direction.EAST)) {
-            this.polygons[idx++] = new ModelPart.Polygon(new ModelPart.Vertex[]{vertex6, vertex2, vertex3, vertex7}, y, ad, aa, ae, q, r, bl, Direction.EAST);
-        }
-
-        if (set.contains(Direction.SOUTH)) {
-            this.polygons[idx] = new ModelPart.Polygon(new ModelPart.Vertex[]{vertex5, vertex6, vertex7, vertex8}, aa, ad, ab, ae, q, r, bl, Direction.SOUTH);
-        }
-    }
-
-    public void transformVertices(Matrix4f matrix) {
-        //Transform original vertices and store them
-        for(int i = 0; i < 8; ++i) {
-            this.vertices[i].mulPosition(matrix, this.transformed[i]);
-        }
-    }
-
-    public ModelPart.Polygon[] getPolygons() { return this.polygons; }
-                }
+        
