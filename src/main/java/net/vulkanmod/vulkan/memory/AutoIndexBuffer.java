de1eca8c9d83a990f1f616af5265eb0961e6e53d
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-18 21:40:49 -0300

    Update AutoIndexBuffer.java

--- src/main/java/net/vulkanmod/vulkan/memory/AutoIndexBuffer.java
+++ src/main/java/net/vulkanmod/vulkan/memory/AutoIndexBuffer.java
@@ -1,71 +1,136 @@
-package net.vulkanmod.vulkan.shader.layout;
+package net.vulkanmod.vulkan.memory;
 
-import net.vulkanmod.vulkan.shader.descriptor.UBO;
+import org.lwjgl.system.MemoryUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
 
-public abstract class AlignedStruct {
+public class AutoIndexBuffer {
+    int vertexCount;
+    final DrawType drawType;
+    IndexBuffer indexBuffer;
 
-    protected final List<Field> fields = new ArrayList<>();
-    protected final int size;
+    public AutoIndexBuffer(int vertexCount, DrawType type) {
+        this.drawType = type;
 
-    protected AlignedStruct(List<Field.FieldInfo> infoList, int size) {
-        this.size = size;
-
-        if (infoList == null) return;
-
-        for (Field.FieldInfo fieldInfo : infoList) {
-            Field field = Field.createField(fieldInfo);
-            this.fields.add(field);
-        }
-
-        // Sort fields by size (assuming compareSizes method exists)
-        this.fields.sort(Field::compareSizes);
+        createIndexBuffer(vertexCount);
     }
 
-    public void update(long ptr) {
-        for (Field field : this.fields) {
-            field.update(ptr);
+    private void createIndexBuffer(int vertexCount) {
+        this.vertexCount = vertexCount;
+        int size;
+        ByteBuffer buffer;
+
+        switch (drawType) {
+            case QUADS -> {
+                size = vertexCount * 3 / 2 * IndexBuffer.IndexType.SHORT.size;
+                buffer = genQuadIdxs(vertexCount);
+            }
+            case TRIANGLE_FAN -> {
+                size = (vertexCount - 2) * 3 * IndexBuffer.IndexType.SHORT.size;
+                buffer = genTriangleFanIdxs(vertexCount);
+            }
+            case TRIANGLE_STRIP -> {
+                size = (vertexCount - 2) * 3 * IndexBuffer.IndexType.SHORT.size;
+                buffer = genTriangleStripIdxs(vertexCount);
+            }
+            default -> throw new RuntimeException("unknown drawType");
+        }
+
+        indexBuffer = new IndexBuffer(size, MemoryTypes.GPU_MEM);
+        indexBuffer.copyBuffer(buffer);
+
+        MemoryUtil.memFree(buffer);
+    }
+
+    public void checkCapacity(int vertexCount) {
+        if(vertexCount > this.vertexCount) {
+            int newVertexCount = this.vertexCount * 2;
+            System.out.println("Reallocating AutoIndexBuffer from " + this.vertexCount + " to " + newVertexCount);
+
+            //TODO: free old
+            //Can't know when VBO will stop using it
+            indexBuffer.freeBuffer();
+            createIndexBuffer(newVertexCount);
         }
     }
 
-    public List<Field> getFields() {
-        return this.fields;
+    public static ByteBuffer genQuadIdxs(int vertexCount) {
+        //short[] idxs = {0, 1, 2, 0, 2, 3};
+
+        int indexCount = vertexCount * 3 / 2;
+        ByteBuffer buffer = MemoryUtil.memAlloc(indexCount * Short.BYTES);
+        ShortBuffer idxs = buffer.asShortBuffer();
+        //short[] idxs = new short[indexCount];
+
+        int j = 0;
+        for(int i = 0; i < vertexCount; i += 4) {
+
+            idxs.put(j, (short) i);
+            idxs.put(j + 1, (short) (i + 1));
+            idxs.put(j + 2, (short) (i + 2));
+            idxs.put(j + 3, (short) (i));
+            idxs.put(j + 4, (short) (i + 2));
+            idxs.put(j + 5, (short) (i + 3));
+
+            j += 6;
+        }
+
+        return buffer;
+        //this.type.copyIndexBuffer(this, bufferSize, idxs);
     }
 
-    public int getSize() {
-        return size;
+    public static ByteBuffer genTriangleFanIdxs(int vertexCount) {
+        int indexCount = (vertexCount - 2) * 3;
+        ByteBuffer buffer = MemoryUtil.memAlloc(indexCount * Short.BYTES);
+        ShortBuffer idxs = buffer.asShortBuffer();
+
+        //short[] idxs = byteBuffer.asShortBuffer().array();
+
+        int j = 0;
+        for (int i = 0; i < vertexCount - 2; ++i) {
+//            idxs[j] = 0;
+//            idxs[j + 1] = (short) (i + 1);
+//            idxs[j + 2] = (short) (i + 2);
+
+            idxs.put(j, (short) 0);
+            idxs.put(j + 1, (short) (i + 1));
+            idxs.put(j + 2, (short) (i + 2));
+
+            j += 3;
+        }
+
+        buffer.rewind();
+        return buffer;
     }
 
-    public static class Builder {
+    public static ByteBuffer genTriangleStripIdxs(int vertexCount) {
+        int indexCount = (vertexCount - 2) * 3;
 
-        final List<Field.FieldInfo> fields = new ArrayList<>();
-        protected int currentOffset = 0;
+        //TODO: free buffer
+        ByteBuffer buffer = MemoryUtil.memAlloc(indexCount * Short.BYTES);
+        ShortBuffer idxs = buffer.asShortBuffer();
 
-        public void addFieldInfo(String type, String name, int count) {
-            Field.FieldInfo fieldInfo = Field.createFieldInfo(type, name, count);
+        //short[] idxs = byteBuffer.asShortBuffer().array();
 
-            // Pre-compute alignment offset
-            fieldInfo.alignmentOffset = fieldInfo.computeAlignmentOffset(currentOffset);
-            currentOffset = fieldInfo.alignmentOffset + fieldInfo.size;
-            fields.add(fieldInfo);
+        int j = 0;
+        for (int i = 0; i < vertexCount - 2; ++i) {
+//            idxs[j] = 0;
+//            idxs[j + 1] = (short) (i + 1);
+//            idxs[j + 2] = (short) (i + 2);
+
+            idxs.put(j, (short) i);
+            idxs.put(j + 1, (short) (i + 1));
+            idxs.put(j + 2, (short) (i + 2));
+
+            j += 3;
         }
 
-        public void addFieldInfo(String type, String name) {
-            addFieldInfo(type, name, 1);
-        }
-
-        public UBO buildUBO(int binding, int stages) {
-            return new UBO(binding, stages, currentOffset * 4, fields);
-        }
-
-        public PushConstants buildPushConstant() {
-            if (fields.isEmpty()) return null;
-
-            return new PushConstants(fields, currentOffset * 4);
-        }
+        buffer.rewind();
+        return buffer;
     }
+
+    public IndexBuffer getIndexBuffer() { return indexBuffer; }
 
     public enum DrawType {
         QUADS(7),
