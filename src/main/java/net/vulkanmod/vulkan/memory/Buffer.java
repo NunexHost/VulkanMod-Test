5b3582151f7d25ab42be10c9df8be9ef89df8837
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-20 10:46:31 -0300

    Update Buffer.java

--- src/main/java/net/vulkanmod/vulkan/memory/Buffer.java
+++ src/main/java/net/vulkanmod/vulkan/memory/Buffer.java
@@ -24,7 +24,7 @@ public abstract class Buffer {
         this.type.createBuffer(this, bufferSize);
 
         if (this.type.mappable()) {
-            this.data = new PointerBuffer(bufferSize); // Use the constructor with int capacity
+            this.data = PointerBuffer.allocate(bufferSize); // Use the allocate() method
         }
     }
 
