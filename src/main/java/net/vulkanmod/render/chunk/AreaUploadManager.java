df98c3cfd6961e0d8312c808c90813cdd75618cf
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-26 22:39:29 -0300

    Update AreaUploadManager.java

--- src/main/java/net/vulkanmod/render/chunk/AreaUploadManager.java
+++ src/main/java/net/vulkanmod/render/chunk/AreaUploadManager.java
@@ -1,11 +1,11 @@
 package net.vulkanmod.render.chunk;
 
 import it.unimi.dsi.fastutil.longs.Long2ObjectArrayMap;
-import it.unimi.dsi.fastutil.objects.ObjectArrayFIFOQueue;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.vulkanmod.Initializer;
 import net.vulkanmod.vulkan.*;
 import net.vulkanmod.vulkan.memory.StagingBuffer;
+import net.vulkanmod.vulkan.memory.StagingBufferPool;
 import net.vulkanmod.vulkan.queue.CommandPool;
 
 import org.lwjgl.system.MemoryStack;
@@ -29,8 +29,6 @@ public class AreaUploadManager {
     ObjectArrayList<AreaBuffer.Segment>[] recordedUploads;
     CommandPool.CommandBuffer[] commandBuffers;
 
-//    LongOpenHashSet dstBuffers = new LongOpenHashSet();
-
     final Long2ObjectArrayMap<ObjectArrayFIFOQueue<SubCopyCommand>> subCopyCommands = new Long2ObjectArrayMap<>();
 
     int currentFrame;
@@ -44,23 +42,22 @@ public class AreaUploadManager {
         }
     }
 
-    public void swapBuffers(long srcBuffer, long dstBuffer)
-    {        hasBufferSwap=true;
-        if(!this.subCopyCommands.containsKey(srcBuffer)) return;
+    public void swapBuffers(long srcBuffer, long dstBuffer) {
+        hasBufferSwap = true;
+        if (!this.subCopyCommands.containsKey(srcBuffer)) return;
         this.subCopyCommands.put(dstBuffer, this.subCopyCommands.remove(srcBuffer));
-
-
     }
+
     public synchronized void submitUploads() {
         if (subCopyCommands.isEmpty()) {
             return;
         }
-        if(commandBuffers[currentFrame] == null)
+        if (commandBuffers[currentFrame] == null)
             this.commandBuffers[currentFrame] = FakeTransferQueue.beginCommands();
         try (MemoryStack stack = MemoryStack.stackPush()) {
             FakeTransferQueue.PriorWriteBarrier(this.commandBuffers[currentFrame].getHandle());
 
-            long stagingBufferId = Vulkan.getStagingBuffer().getId();
+            long stagingBufferId = Vulkan.getStagingBufferPool().getId();
             for (long bufferHandle : subCopyCommands.keySet()) {
                 final ObjectArrayFIFOQueue<SubCopyCommand> subCopyCommands1 = subCopyCommands.get(bufferHandle);
 
@@ -75,40 +72,22 @@ public class AreaUploadManager {
                 FakeTransferQueue.uploadBufferCmds(this.commandBuffers[currentFrame], stagingBufferId, bufferHandle, vkBufferCopies);
             }
             FakeTransferQueue.UploadCmdWriteBarrier(this.commandBuffers[currentFrame].getHandle(), stack, this.hasBufferSwap);
-            this.hasBufferSwap=false;
+            this.hasBufferSwap = false;
         }
         subCopyCommands.clear();
         FakeTransferQueue.submitCommands(this.commandBuffers[currentFrame]);
 
     }
-//    public void uploadAsync2(AreaBuffer.Segment uploadSegment, long bufferId, int dstOffset, int bufferSize, long src) {
-//
-//        StagingBuffer stagingBuffer = Vulkan.getStagingBuffer();
-//        stagingBuffer.copyBuffer2(bufferSize, src);
-//
-//
-//        if(!subCopyCommands.containsKey(bufferId))
-//        {
-//            subCopyCommands.put(bufferId, new ObjectArrayFIFOQueue<>(16));
-//        }
-//        subCopyCommands.get(bufferId).enqueue(new SubCopyCommand(stagingBuffer.getOffset(), dstOffset, bufferSize));
-//
-//
-//        this.recordedUploads[this.currentFrame].add(uploadSegment);
-//    }
+
     public void uploadAsync(AreaBuffer.Segment uploadSegment, long bufferId, int dstOffset, int bufferSize, ByteBuffer src) {
-
-
-        StagingBuffer stagingBuffer = Vulkan.getStagingBuffer();
+        // Utilize um pool de buffers de upload para reduzir alocações frequentes.
+        StagingBuffer stagingBuffer = Vulkan.getStagingBufferPool().getBuffer(bufferSize);
         stagingBuffer.copyBuffer(bufferSize, src);
 
-
-        if(!subCopyCommands.containsKey(bufferId))
-        {
+        if (!subCopyCommands.containsKey(bufferId)) {
             subCopyCommands.put(bufferId, new ObjectArrayFIFOQueue<>(12));
         }
         subCopyCommands.get(bufferId).enqueue(new SubCopyCommand(stagingBuffer.getOffset(), dstOffset, bufferSize));
-
 
         this.recordedUploads[this.currentFrame].add(uploadSegment);
     }
@@ -116,19 +95,15 @@ public class AreaUploadManager {
     public void updateFrame() {
         this.currentFrame = (this.currentFrame + 1) % FRAME_NUM;
         waitUploads(this.currentFrame);
-
     }
 
-    public void waitUploads() {
-        this.waitUploads(this.currentFrame);
-    }
-    private void waitUploads(int frame) {
+    public void waitUploads(int frame) {
         CommandPool.CommandBuffer commandBuffer = commandBuffers[frame];
-        if(commandBuffer == null)
+        if (commandBuffer == null)
             return;
         Synchronization.waitFence(commandBuffers[frame].getFence());
 
-        for(AreaBuffer.Segment uploadSegment : this.recordedUploads[frame]) {
+        for (AreaBuffer.Segment uploadSegment : this.recordedUploads[frame]) {
             uploadSegment.setReady();
         }
 
@@ -138,13 +113,13 @@ public class AreaUploadManager {
     }
 
     public synchronized void waitAllUploads() {
-        for(int i = 0; i < this.commandBuffers.length; ++i) {
+        for (int i = 0; i < this.commandBuffers.length; ++i) {
             waitUploads(i);
         }
     }
 
     public void copyBuffer(long srcBuffer, long dstBuffer, int bufferSize) {
-        if(commandBuffers[currentFrame] == null)
+        if (commandBuffers[currentFrame] == null)
             this.commandBuffers[currentFrame] = FakeTransferQueue.beginCommands();
         FakeTransferQueue.uploadBufferCmd(this.commandBuffers[currentFrame], srcBuffer, 0, dstBuffer, 0, bufferSize);
     }
